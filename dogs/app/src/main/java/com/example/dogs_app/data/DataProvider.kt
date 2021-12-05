4ded7f61614f51523dc4ad91de60ae6185ce5b9a
MaxVolokh <mwell12.mw@gmail.com>
2021-12-05 17:11:26 +0200

    Lab_Dogs

---
+++ dogs/app/src/main/java/com/example/dogs_app/data/DataProvider.kt
@@ -0,0 +1,105 @@
+package com.example.dogs_app.data
+
+import com.example.dogs_app.R
+
+object DataProvider {
+
+    val puppyList = listOf(
+        Puppy(
+            id = 1,
+            title = "Olimp",
+            sex = "Male",
+            age = 7,
+            description = "Patient and affectionate towards children. Very loyal to the owner.",
+            puppyImageId = R.drawable.p1
+        ),
+        Puppy(
+            id = 2,
+            title = "Barbos",
+            sex = "Male",
+            age = 8,
+            description = "It adapts well to all weather conditions.",
+            puppyImageId = R.drawable.p2
+        ),
+        Puppy(
+            id = 3,
+            title = "Greg",
+            sex = "Male",
+            age = 3,
+            description = "He is quite capricious and needs the right upbringing.",
+            puppyImageId = R.drawable.p3
+        ),
+        Puppy(
+            id = 4,
+            title = "Karat",
+            sex = "Male",
+            age = 6,
+            description = "This dog is distinguished by perseverance, willpower and courage.",
+            puppyImageId = R.drawable.p4
+        ),
+        Puppy(
+            id = 5,
+            title = "Nilson",
+            sex = "Male",
+            age = 6,
+            description = "He is incredibly loyal to his master and greatly yearns in separation from him.",
+            puppyImageId = R.drawable.p5
+        ),
+        Puppy(
+            id = 6,
+            title = "Ginnes",
+            sex = "Male",
+            age = 9,
+            description = "He gets along well with children, taking care of them and protecting them in every possible way.",
+            puppyImageId = R.drawable.p6
+        ),
+        Puppy(
+            id = 7,
+            title = "Omelia",
+            sex = "Female",
+            age = 4,
+            description = "Able to walk for a long time. They are very mobile, but if necessary, they can quickly concentrate for work.",
+            puppyImageId = R.drawable.p7
+        ),
+        Puppy(
+            id = 8,
+            title = "Taffi",
+            sex = "Female",
+            age = 7,
+            description = "Bichon Frize is a harmoniously developed breed with strong legs and a solid build",
+            puppyImageId = R.drawable.p8
+        ),
+        Puppy(
+            id = 9,
+            title = "Kira",
+            sex = "Female",
+            age = 8,
+            description = "The character of bobtails is balanced and calm, they are reasonable and independent.",
+            puppyImageId = R.drawable.p9
+        ),
+        Puppy(
+            id = 10,
+            title = "Ivi",
+            sex = "Female",
+            age = 11,
+            description = "Boxers are the owners of a strong, balanced psyche and an agreeable character.",
+            puppyImageId = R.drawable.p10
+        ),
+        Puppy(
+            id = 11,
+            title = "Rimma",
+            sex = "Female",
+            age = 5,
+            description = "The Russian Greyhound is a large long-haired dog with an elegant proportional build.",
+            puppyImageId = R.drawable.p11
+        ),
+        Puppy(
+            id = 12,
+            title = "Rita",
+            sex = "Female",
+            age = 8,
+            description = "The Bull Terrier is an active and agile dog, it can withstand great physical exertion.",
+            puppyImageId = R.drawable.p12
+        ),
+    )
+}
